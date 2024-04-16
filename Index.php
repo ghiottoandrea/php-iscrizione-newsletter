d7c35cc62ca1651f97e9cb12f86a65d1e4b9f276
ghiottoandrea <andreaghiotto9@gmail.com>
2024-04-16 16:24:44 +0200

    first commit

---
+++ Index.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+# iscrizione ad una newsletter.
+
+
+
+1. Controllare che la mail passata in GET sia ben formata e contenga un punto e una chiocciola.
+
+
+2. Se è corretta stampare un messaggio di success in un alert di Bootstrap, altrimenti (sempre in un alert di Bootstrap) mostrare un messaggio di errore.
+
+
+
+**Milestone 1**:
+scriviamo tutto (logica e layout) in un unico file *index.php*
+
+**Milestone 2:**
+verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file *functions.php* che includeremo poi nella pagina principale
+
+Aggiungere anche un po’ di stile alla pagina tramite Bootstrap.
+
+Cosa controlla isset? 
+qualsiasi valore diverso da null.
+
+*/
+?>
