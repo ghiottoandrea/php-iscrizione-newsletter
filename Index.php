8f3e73a1572bb211e6ee8f461096aff0107c5e30
ghiottoandrea <andreaghiotto9@gmail.com>
2024-04-22 16:59:19 +0200

    Add newletter check

--- Index.php
+++ Index.php
@@ -23,4 +23,62 @@ Cosa controlla isset?
 qualsiasi valore diverso da null.
 
 */
+$email_address = $_GET['email'];
+if (isset($_GET['email'])) {
+
+    $alert = checkEmailAddress($email_address);
+}
+
+function checkEmailAddress(string $email): array
+{
+
+    if (str_contains($email, '@') and str_contains($email, '.')) {
+
+        $alert = [
+            'color' => 'success',
+            'message' => 'Thanks for joining our newsletter!'
+        ];
+    } else {
+        $alert = [
+            'color' => 'danger',
+            'message' => 'Email Address format incorrect!'
+        ];
+    }
+
+    return $alert;
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <form>
+            <div class="input-group mb-3">
+                <input name="email" id="email" type="text" class="form-control" placeholder="Your email address" aria-label="Your email address" aria-describedby="button-addon2">
+                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Join</button>
+            </div>
+        </form>
+    </div>
+
+
+    <?php if (isset($alert) || $alert !== null) : ?>
+        <div class="container my-3">
+            <div class="alert alert-<?= $alert['color']; ?>" role="alert">
+                <strong>Newsletter: </strong> <?= $alert['message']; ?>
+            </div>
+        </div>
+    <?php endif; ?>
+
+</body>
+
+</html>
